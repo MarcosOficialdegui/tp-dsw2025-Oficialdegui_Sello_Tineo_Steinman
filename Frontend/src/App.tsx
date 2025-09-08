a251937e9476e5ba8766418af9981f4316c29222
Tomás Tineo <82066164+TomasTineo@users.noreply.github.com>
2025-09-08 19:51:29 -0300

    detallitos

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import UsuarioFormRegistro from './pages/UsuarioFormRegistro';
 import UsuarioFormLogin from './pages/UsuarioFormLogin';
+import Complejo from './pages/Complejo';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/registrarse" element={<UsuarioFormRegistro />} />
         <Route path="/login" element={<UsuarioFormLogin />} />
+        <Route path="/complejo/:id" element={<Complejo />} />
       </Routes>
       </BrowserRouter>
     </>
