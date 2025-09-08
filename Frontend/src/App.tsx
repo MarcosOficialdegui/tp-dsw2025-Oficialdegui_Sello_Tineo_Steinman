6a0c0cb5bca0fa705c190156c0ed188edfc542f3
valen <valen@DESKTOP-GQP6OPG>
2025-09-08 20:41:49 -0300

    arreglo de merge

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,11 +2,11 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import UsuarioFormRegistro from './pages/UsuarioFormRegistro';
 import UsuarioFormLogin from './pages/UsuarioFormLogin';
-<<<<<<< HEAD
+
 import Perfil from './pages/Perfil';
-=======
+
 import Complejo from './pages/Complejo';
->>>>>>> a251937e9476e5ba8766418af9981f4316c29222
+
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -19,12 +19,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/registrarse" element={<UsuarioFormRegistro />} />
-        <Route path="/login" element={<UsuarioFormLogin />} />
-<<<<<<< HEAD
+        <Route path="/login" element={<UsuarioFormLogin />} /> 
+
         <Route path="/perfil" element={<Perfil />} />
-=======
+
         <Route path="/complejo/:id" element={<Complejo />} />
->>>>>>> a251937e9476e5ba8766418af9981f4316c29222
+
       </Routes>
       </BrowserRouter>
     </>
